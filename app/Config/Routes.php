772129b01ceace0b2dabf3753ad49e1867064af2
unknown <dwikapangestuti999@gmail.com>
2023-10-30 23:47:23 +0700

    login page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,11 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\LoginController;
+
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/login', [LoginController::class, 'index']);
